ce38acb07adca24e40350daf6ba6fee224ed4fca
SimeonHristov99 <s.e.hristov99@gmail.com>
2020-06-04 11:33:24 +0300

    moreTests

--- Haskell Classes/Homework_3/task1.hs
+++ Haskell Classes/Homework_3/task1.hs
@@ -14,28 +14,28 @@ main = do
 
 ------------------------------------------------------------------------------------------
 
-data BTree = Empty | Node Char BTree BTree
+data BTree a = Empty | Node a (BTree a) (BTree a)
  deriving (Show)
 
-t1 :: BTree
+t1 :: BTree Char
 t1 = Node 'a' (Node 'c' (Node 'f' Empty Empty) (Node 'd' Empty Empty)) (Node 'b' Empty (Node 'e' Empty Empty))
 
-t2 :: BTree
+t2 :: BTree Char
 t2 = Node 'a' (Node 'c' (Node 'd' Empty Empty) Empty) (Node 'b' Empty Empty)
 
-t3 :: BTree
+t3 :: BTree Char
 t3 = Node 'a' (Node 'b' (Node 'd' (Node 'h' Empty Empty) (Node 'i' Empty Empty)) (Node 'e' Empty Empty)) (Node 'c' (Node 'f' Empty Empty) (Node 'g' Empty Empty)) 
 
 ------------------------------------------------------------------------------------------
 
-containsWord :: BTree -> [Char] -> Bool
+containsWord :: (Eq a) => BTree a -> [a] -> Bool
 containsWord Empty _ = False
 containsWord _ [] = True
 containsWord (Node value left right) (x:xs)
  | value == x = helper left xs || helper right xs
  | otherwise = containsWord left (x:xs) || containsWord right (x:xs)
  
-helper :: BTree -> [Char] -> Bool
+helper :: (Eq a) => BTree a -> [a] -> Bool
 helper Empty [] = True
 helper (Node value left right) (x:xs)
  | value == x = helper left xs || helper right xs
--- Haskell Classes/Homework_3/task3.hs
+++ Haskell Classes/Homework_3/task3.hs
@@ -9,44 +9,44 @@ main = do
 
 ------------------------------------------------------------------------------------------
 
-data BTree = Empty | Node Char BTree BTree
+data BTree a = Empty | Node a (BTree a) (BTree a)
  deriving (Show)
 
-t1 :: BTree
+t1 :: BTree Char
 t1 = Node 'a' (Node 'c' (Node 'f' Empty Empty) (Node 'd' Empty Empty)) (Node 'b' Empty (Node 'e' Empty Empty))
 
-t2 :: BTree
+t2 :: BTree Char
 t2 = Node 'a' (Node 'c' (Node 'd' Empty Empty) Empty) (Node 'b' Empty Empty)
 
-t3 :: BTree
+t3 :: BTree Char
 t3 = Node 'a' (Node 'b' (Node 'd' (Node 'h' Empty Empty) (Node 'i' Empty Empty)) (Node 'e' Empty Empty)) (Node 'c' (Node 'f' Empty Empty) (Node 'g' Empty Empty)) 
 
-t4 :: BTree
+t4 :: BTree Char
 t4 = Node 'a' (Node 'c' (Node 'd' Empty Empty) Empty) (Node 'g' Empty Empty)
 
 ------------------------------------------------------------------------------------------
 
-allContain :: [BTree] -> [String]
+allContain :: (Eq a) => [BTree a] -> [[a]]
 allContain [] = []
 allContain xs = foldr1 intersect $ map genWords xs
 
 ------------------------------------------------------------------------------- tasks 1, 2
 
-containsWord :: BTree -> [Char] -> Bool
+containsWord :: (Eq a) => BTree a -> [a] -> Bool
 containsWord Empty _ = False
 containsWord _ [] = True
 containsWord (Node value left right) (x:xs)
  | value == x = helper left xs || helper right xs
  | otherwise = containsWord left (x:xs) || containsWord right (x:xs)
  
-helper :: BTree -> [Char] -> Bool
+helper :: (Eq a) => BTree a -> [a] -> Bool
 helper Empty [] = True
 helper (Node value left right) (x:xs)
  | value == x = helper left xs || helper right xs
  | otherwise = False
 helper _ _ = False
 
-genWords :: BTree -> [String]
+genWords :: (Eq a) => BTree a -> [[a]]
 genWords Empty = []
 genWords (Node value Empty Empty) = [[value]]
 genWords tree@(Node value left right) = filter (containsWord tree) $ map (value:) (genWords left)
