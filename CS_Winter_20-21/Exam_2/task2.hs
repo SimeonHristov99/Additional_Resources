90e9d2648ccd6f8ce38c276dbbad03e5c958553e
SimeonHristov99 <s.e.hristov99@gmail.com>
2021-01-17 18:58:44 +0200

    adding sols ai_hw_3

--- CS_Winter_20-21/Exam_2/task2.hs
+++ CS_Winter_20-21/Exam_2/task2.hs
@@ -1,8 +1,10 @@
 main :: IO()
 main = do
-    print $ dominates (+4) (*2) [] == True
     print $ dominates (+4) (*2) [1..4] == True
     print $ dominates (+4) (*2) [1..5] == False -- 5 + 4 = 9 < 5 * 2 = 10
+    print $ dominates (*3) (*2) [1..10] == True
+    print $ dominates (*2) (+4) [1..5] == False
+    print $ dominates (*(-5)) (+4) [1..5] == True
 
 dominates :: (Int -> Int) -> (Int -> Int) -> [Int] -> Bool
 dominates f g xs = all (\ (x, y) -> (abs x) >= (abs y)) $ zip (map f xs) (map g xs)
