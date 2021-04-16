beb1cd2e632c5e6c66d4478b548a7e76f33d779a
SimeonHristov99 <s.e.hristov99@gmail.com>
2021-04-16 08:07:11 +0300

    correct

--- IS_Summer_20-21/Week_08 - Algebraic Types. Nonlinear Data Structures/task3.hs
+++
@@ -1,9 +0,0 @@
-main :: IO()
-main = do
-    print $ Circle 5 == Circle 10
-    print $ Rectangle 5 20
-    print $ Triangle 5 3 9
-    print $ Cylinder 5 12
-
-data Shape = Circle Int | Rectangle Int Int | Triangle Int Int Int | Cylinder Int Int
- deriving (Show, Eq)
